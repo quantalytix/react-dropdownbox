33d40c82c56b0a32b4323cbde69ef1c1769b9ed6
Christopher Aliotta <chrisaliotta@quantalytix.com>
2019-01-12 17:42:50 -0600

    created internal indexes and data states

--- src/dropdown/ReactDropdown.jsx
+++ src/dropdown/ReactDropdown.jsx
@@ -6,70 +6,45 @@ export default class ReactDropdown extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      dropdownVisible: false,
-      textInput: 'enter text here',
-      activeIndex: 0,
-      itemStack: []
-    }
-
     this.handleOnSelected = this.handleOnSelected.bind(this);
     this.handleOnBlur = this.handleOnBlur.bind(this);
     this.handleOnFocus = this.handleOnFocus.bind(this);
     this.handleOnChange = this.handleOnChange.bind(this);
     this.handleExitClick = this.handleExitClick.bind(this);
 
-    //console.log(this.flattenDeep(this.props.data));
-    /*let wrapped = [];
-    wrapped = this.wrapItemArray(this.props.data);
-    console.log(wrapped);
-    let indexed = [];
-    */
-    console.log(this.props.data);
-    let indexed = this.mutateArrayAddIndexes2(this.props.data);
-    console.log(indexed.array);
-    console.log(indexed.count);
+    let result = this.createInternalItemArray(this.props.data);
+    let internalData = result.array;
+    let count = result.count;
 
-    console.log(this.flattenDeep(this.props.data));
-    console.log(this.flattenDeep(indexed.array));
+    //let selectables = this.flattenDeep(internalData);
 
-    //let flat = this.flattenDeep(indexed.array);
-    //console.log(flat);
+    this.state = {
+      dropdownVisible: false,
+      textInput: 'enter text here',
+      activeIndex: 0,
+      //itemStack: selectables,
+      count: count,
+      internalData: internalData
+    }
   }
 
   flattenDeep(arr1) {
-    // flatten object array to include only items and not group objects
+    // flatten object array to include items and identify group objects in 
+    // a single dimension array
     let array = arr1.reduce((acc, val) => Array.isArray(val.children) ? acc.concat({ node: val.node, isGroup: true, key: val.key, value: val.value}, this.flattenDeep(val.children)) : acc.concat({ node: val.node, key: val.key, value: val }), []);
-    // index each item object in the array with a unique number
-    //for (let i = 0; i < array.length; i++) array[i].node = i;
-    // return the flattened indexed array of selectable items
     return array;
   }
 
-
-  mutateArrayAddIndexes(arr1, count = 0) {
-    for (let i = 0; i < arr1.length; i++) {
-      arr1[i].node = {};
-      arr1[i].node = count;
-      count = count + 1;
-      if (Array.isArray(arr1[i].children)) {
-        var result = this.mutateArrayAddIndexes(arr1[i].children, count);
-        arr1[i].children = result.array;
-        count = result.count;
-      }
-    }
-    return { array: arr1, count: count };
-  }
-
-  mutateArrayAddIndexes2(arr1, count = 0) {
+  createInternalItemArray(arr1, count = 0) {
     let array = []
     for (let i = 0; i < arr1.length; i++) {
       let item = {node: NaN, children: NaN, value: NaN, key: NaN};
       item.node = count;
       item.value = arr1[i].value;
+      item.key = arr1[i].key;
       count = count + 1;
       if (Array.isArray(arr1[i].children)) {
-        var result = this.mutateArrayAddIndexes2(arr1[i].children, count);
+        var result = this.createInternalItemArray(arr1[i].children, count);
         item.children = result.array;
         count = result.count;
       }
@@ -79,7 +54,7 @@ export default class ReactDropdown extends Component {
   }
 
   searchController({ textInput, dispatch }) {
-    let wrappedItemArray = this.flattenDeep(this.props.data);
+    //let wrappedItemArray = this.flattenDeep(this.props.data);
     return this.renderItems(this.props.data, dispatch, 0)
   }
 
