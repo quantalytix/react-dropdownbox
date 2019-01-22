69260bfb0fbd226e0b29d11843d6df3dcf9dd8ec
Jennifer Nelson <jennifernelson118@gmail.com>
2019-01-22 08:38:53 -0600

    ReactDropdownFilter: converted from js class to individual functions; only exported needed function
    ReactDropdown: filterList is default filter prop

--- src/dropdown/ReactDropdownFilter.js
+++ src/dropdown/ReactDropdownFilter.js
@@ -1,26 +1,20 @@
 
-class ReactDropdownFilter {
-
-  constructor(){
-    this.filterList = this.filterList.bind(this);
-  }
-
   //both list and search are arrays for parameters
-  filterList(list, search) {
+  const filterList = (list, search) => {
     let results = [];
     for (let i = 0; i < list.length; i++) {
       if (list[i].children) {
         let objCopy = Object.assign({}, list[i]);        
-        objCopy.children = this.filterList(list[i].children, search);
+        objCopy.children = filterList(list[i].children, search);
         if (objCopy.children.length > 0) results.push(objCopy);
       }
       else {
         for (let n = 0; n < search.length; n++) {
           if (search[n] !== '') {
-            let regExp = this.buildRegEx(search[n]);
-            if (this.checkIfMatch(list[i], regExp)) {
+            let regExp = buildRegEx(search[n]);
+            if (checkIfMatch(list[i], regExp)) {
               // need to check if results are already in the list
-              if (!this.checkDuplicate(list[i], results)) {
+              if (!checkDuplicate(list[i], results)) {
                 results.push(list[i]);
               }
             }
@@ -31,20 +25,20 @@ class ReactDropdownFilter {
     return results;
   }
 
-  checkIfMatch(item, regExp) {
+  const checkIfMatch = (item, regExp) => {
     return item.text.match(regExp);
   }
 
-  buildRegEx(str) {
+  const buildRegEx = (str) => {
     let sanitized = str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
     return new RegExp('^' + sanitized + '', "gmi");
   }
 
-  checkDuplicate(item, list) {
+  const checkDuplicate = (item, list) => {
     for (let i = 0; i < list.length; i++) {
       if (list[i] === item) return true;
     }
     return false;
   }
-}
-export { ReactDropdownFilter };
+  
+export { filterList };
